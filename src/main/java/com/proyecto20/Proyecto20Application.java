492740936b9919d961493da51b120e2de7c48df0
RobertoAnguloDeveloper <rangulot@outlook.com>
2022-10-14 20:25:58 -0500

    First commit

---
+++ src/main/java/com/proyecto20/Proyecto20Application.java
@@ -0,0 +1,13 @@
+package com.proyecto20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto20Application.class, args);
+    }
+
+}
